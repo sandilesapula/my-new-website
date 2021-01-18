e51c0dadaaf7ba59463271b8513b026dd7e0fbfd
sandilesapula <davidsandilesapula@gmail.com>
2021-01-18 15:16:43 +0200

    Developed by fullname

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
         <p className="footer__text">
           © {new Date().getFullYear()} - Template developed by{' '}
           <a href="https://github.com/sandilesapula" target="_blank" rel="noopener noreferrer">
-            Sandile Sapula
+            David Sandile Sapula
           </a>
         </p>
 
